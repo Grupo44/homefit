ad6879ddd9c3702c221dc4bbfc75a524ee684256
Grupo44 <henrique.salves@a.ficr.edu.br>
2021-11-05 13:21:26 -0300

    Melhoria na tela de Login

--- app/src/main/java/com/project/homefit/FormLogin.kt
+++ app/src/main/java/com/project/homefit/FormLogin.kt
@@ -2,10 +2,17 @@ package com.project.homefit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        setContentView(R.layout.activity_form_login);
+//        getSupportActionBar().hide();
     }
+
+    override fun getSupportActionBar(): ActionBar? {
+        return super.getSupportActionBar();
+    }
+
 }
