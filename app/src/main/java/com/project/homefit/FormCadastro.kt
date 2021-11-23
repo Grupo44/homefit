ff671d1c9205560cfda1887c6e3b90ca508e6783
Grupo44 <henrique.salves@a.ficr.edu.br>
2021-11-22 21:01:49 -0300

    adicionando os id dos imputs

--- app/src/main/java/com/project/homefit/FormCadastro.kt
+++ app/src/main/java/com/project/homefit/FormCadastro.kt
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class FormCadastro : AppCompatActivity() {
+
+//    private EditText edit_nome, edit_email, edit_senha, edit_idade, edit_altura, edit_peso;
+//    private Button bt_cadastrar;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
 
 //        getSupportActionBar().hide();
+//        IniciarComponentes();
     }
+
+//    private void IniciarComponentes(){
+//        edit_nome = findViewById(R.id.edit_nome);
+//        edit_email = findViewById(R.id.edit_email);
+//        edit_senha = findViewById(R.id.edit_senha);
+//        edit_idade = findViewById(R.id.edit_idade);
+//        edit_altura = findViewById(R.id.edit_altura);
+//        edit_peso = findViewById(R.id.edit_peso);
+//        bt_cadastrar = findViewById(R.id.bt_cadastrar);
+//    }
+
 }
