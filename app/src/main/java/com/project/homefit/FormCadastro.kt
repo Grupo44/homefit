fefba6e8f5b595ecbced4585076c212cb3070a99
Grupo44 <henrique.salves@a.ficr.edu.br>
2021-11-26 20:01:03 -0300

    Validação de input da tela cadastrar

--- app/src/main/java/com/project/homefit/FormCadastro.kt
+++ app/src/main/java/com/project/homefit/FormCadastro.kt
@@ -2,11 +2,13 @@ package com.project.homefit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 
 class FormCadastro : AppCompatActivity() {
 
 //    private EditText edit_nome, edit_email, edit_senha, edit_idade, edit_altura, edit_peso;
 //    private Button bt_cadastrar;
+//    String[] mensagem = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +16,25 @@ class FormCadastro : AppCompatActivity() {
 
 //        getSupportActionBar().hide();
 //        IniciarComponentes();
+
+//        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//
+//                String nome = edit_nome.getText().toString();
+//                String email = edit_email.getText().toString();
+//                String senha = edit_senha.getText().toString();
+//                String idade = edit_idade.getText().toString();
+//                String altura = edit_altura.getText().toString();
+//                String peso = edit_peso.getText().toString();
+//
+//                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()
+//                    || idade.isEmpty() || altura.isEmpty()|| peso.isEmpty()){
+//
+//                    Snackbar snackbar = Snackbar.nake(v, )
+//                }
+//            }
+//        });
     }
 
 //    private void IniciarComponentes(){
