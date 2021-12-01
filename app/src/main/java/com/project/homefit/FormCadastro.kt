599aa549b8cf593bd2861291dd7b9204a9af2464
Grupo44 <henrique.salves@a.ficr.edu.br>
2021-12-01 19:28:23 -0300

    Ajustes na tela de cadastrar

--- app/src/main/java/com/project/homefit/FormCadastro.kt
+++ app/src/main/java/com/project/homefit/FormCadastro.kt
@@ -1,50 +1,55 @@
 package com.project.homefit
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
+import android.widget.EditText;
 
 class FormCadastro : AppCompatActivity() {
 
-//    private EditText edit_nome, edit_email, edit_senha, edit_idade, edit_altura, edit_peso;
-//    private Button bt_cadastrar;
-//    String[] mensagem = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
+    private EditText edit_nome, edit_email, edit_senha, edit_idade, edit_altura, edit_peso;
+    private Button bt_cadastrar;
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        String[] mensagem = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
 
-//        getSupportActionBar().hide();
-//        IniciarComponentes();
+        public void IniciarComponentes() {
+            edit_nome = findViewById<EditText>(R.id.edit_nome);
+            edit_email = findViewById<EditText>(R.id.edit_email);
+            edit_senha = findViewById<EditText>(R.id.edit_senha);
+            edit_idade = findViewById<EditText>(R.id.edit_idade);
+            edit_altura = findViewById<EditText>(R.id.edit_altura);
+            edit_peso = findViewById<EditText>(R.id.edit_peso);
+            bt_cadastrar = findViewById<Button>(R.id.bt_cadastrar);
+        }
 
-//        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v){
-//
-//                String nome = edit_nome.getText().toString();
-//                String email = edit_email.getText().toString();
-//                String senha = edit_senha.getText().toString();
-//                String idade = edit_idade.getText().toString();
-//                String altura = edit_altura.getText().toString();
-//                String peso = edit_peso.getText().toString();
-//
-//                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()
-//                    || idade.isEmpty() || altura.isEmpty()|| peso.isEmpty()){
-//
-//                    Snackbar snackbar = Snackbar.nake(v, )
-//                }
-//            }
-//        });
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                 nome = edit_nome.getText().toString();
+                 email = edit_email.getText().toString();
+                 senha = edit_senha.getText().toString();
+                 idade = edit_idade.getText().toString();
+                 altura = edit_altura.getText().toString();
+                 peso = edit_peso.getText().toString();
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()
+                    || idade.isEmpty() || altura.isEmpty()|| peso.isEmpty()){
+
+                    Snackbar snackbar = Snackbar.nake(v,mensagem[0],Snackbar.LENGTH_SHORT );
+                     snackbar.setBackgroudTint(Color.WHITE);
+
+                }
+            }
+        });
     }
-
-//    private void IniciarComponentes(){
-//        edit_nome = findViewById(R.id.edit_nome);
-//        edit_email = findViewById(R.id.edit_email);
-//        edit_senha = findViewById(R.id.edit_senha);
-//        edit_idade = findViewById(R.id.edit_idade);
-//        edit_altura = findViewById(R.id.edit_altura);
-//        edit_peso = findViewById(R.id.edit_peso);
-//        bt_cadastrar = findViewById(R.id.bt_cadastrar);
-//    }
-
 }
